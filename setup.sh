6eab4b3f90712fabd32963611ecc7fc78762b0f6
BennyYip <yebenmy@protonmail.com>
2017-03-08 19:04:21 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 [ -d ~/.vim/.swapfiles ] || mkdir ~/.vim/.swapfiles
+[ -L ~/.vimrc ] || ln -s ~/.vim/init.vim ~/.vimrc
 
 #sudo pacman -Syyu ripgrep ttf-inziu-iosevka yapf --needed
 pacaur -S universal-ctags-git powerline-fonts ripgrep-broadwell-git ttf-inziu-iosevka yapf --needed
