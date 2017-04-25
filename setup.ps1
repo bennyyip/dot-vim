b50b69b7848c0a4d047d870812333727a0aa9a38
bennyyip <yebenmy@gmail.com>
2017-04-25 10:51:18 +0800

    vim-plug installation script for Windows

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,7 @@
+New-Item -ItemType Directory ~/.vim/autoload
+$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+(New-Object Net.WebClient).DownloadFile($uri, $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("~\.vim\autoload\plug.vim"))
+
 if (-Not (Test-Path .swapfiles)) {
   New-Item -ItemType Directory .swapfiles
 }
