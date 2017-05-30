6554ffc256268ae61ac5401c30f8cd27939c6c85
BennyYip <yebenmy@gmail.com>
2017-05-30 17:54:19 +0800

    refactor

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,5 @@
-New-Item -ItemType Directory ~/.vim/autoload
+﻿New-Item -ItemType Directory ~/.vim/autoload
 $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 (New-Object Net.WebClient).DownloadFile($uri, $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("~\.vim\autoload\plug.vim"))
-
-if (-Not (Test-Path .swapfiles)) {
-  New-Item -ItemType Directory .swapfiles
-}
-
-Copy-Item winvimrc ~/.vimrc
 
 vim +PlugInstall +qall
