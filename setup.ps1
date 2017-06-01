ea20d025a54b4efac448e50f7509a896a4d68de4
bennyyip <yebenmy@gmail.com>
2017-06-01 12:32:26 +0800

    don't ignore plug.vim

--- setup.ps1
+++
@@ -1,5 +0,0 @@
-﻿New-Item -ItemType Directory ~/.vim/autoload
-$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-(New-Object Net.WebClient).DownloadFile($uri, $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("~\.vim\autoload\plug.vim"))
-
-vim +PlugInstall +qall
