2e2c25514d05f4d38acd82bb3dfab2cca53b4a16
BennyYip <yebenmy@gmail.com>
2017-06-01 12:32:26 +0800

    don't ignore plug.vim

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-[ -f ~/.vim/autoload/plug.vim ] ||  \
-  curl -fLo ~/.vim/autoload/plug.vim --create-dir \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
-  echo "vim-plug installed"
-[ -d ~/.vim/.swapfiles ] || mkdir ~/.vim/.swapfiles
 
-#sudo pacman -Syyu ripgrep ttf-inziu-iosevka yapf --needed
 pacaur -S universal-ctags-git powerline-fonts ripgrep-broadwell-git ttf-inziu-iosevka yapf --needed
 
 vim +PlugInstall +qall
