5c44b751d498b7e8c047c9007251443cabd5a04a
bennyyip <yebenmy@protonmail.com>
2023-07-15 00:41:03 +0800

    remove spell

--- spell/download-dict.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-curl -o $HOME/.vim/spell/es.utf-8.spl http://ftp.vim.org/vim/runtime/spell/es.utf-8.spl
-curl -o $HOME/.vim/spell/es.utf-8.sug http://ftp.vim.org/vim/runtime/spell/es.utf-8.sug
